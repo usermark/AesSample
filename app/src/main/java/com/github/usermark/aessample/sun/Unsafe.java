23772a9e0620e24ec2a1efb64c2f57bd274c9875
mike_chen <mike_chen@turn2cloud.com>
2023-07-12 17:27:36 +0800

    增加Java Class

---
+++ app/src/main/java/com/github/usermark/aessample/sun/Unsafe.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.github.usermark.aessample.sun;
+
+import java.nio.ByteBuffer;
+
+
+/**
+ * A collection of methods for performing low-level, unsafe operations.
+ * Although the class and all methods are public, use of this class is
+ * limited because only trusted code can obtain instances of it.
+ *
+ * @author John R. Rose
+ * @see #getUnsafe
+ */
+
+public final class Unsafe {
+
+    private Unsafe() {}
+
+    private static final Unsafe theUnsafe = new Unsafe();
+
+    public static Unsafe getUnsafe() {
+        return theUnsafe;
+    }
+
+    public int getInt(byte[] o, long offset) {
+        ByteBuffer bf = ByteBuffer.wrap(o, (int) offset, 4);
+        return bf.getInt();
+    }
+
+    public void putInt(byte[] o, long offset, int x) {
+        ByteBuffer bf = ByteBuffer.wrap(o);
+        bf.putInt((int) offset, x);
+    }
+}
